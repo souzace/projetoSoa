dee949399c4da5b0dd2d8deb483926af3f125f1f
Fabio Souza <souzace@gmail.com>
2019-12-06 00:22:27 -0300

    crud

--- routes/users.js
+++ routes/users.js
@@ -1,66 +1,123 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const path = require("path");
+const dbPath = path.resolve(__dirname, "../soa.db");
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(":memory:");
-var builder = require('xmlbuilder');
-
-var usersObj = [
-  {id: 1, name: 'fabio', email:'souzace@gmail.com'},
-  {id: 2, name: 'marcio', email:'marcio@gmail.com'},
-  {id: 3, name: 'lidia', email:'lidia@gmail.com'}
-]
-
-var users = builder.create('users', { encoding: 'utf-8' });
-    users.att('xmlns', 'http://www.w3.org/2005/Atom');
-for(var i = 0; i < usersObj.length; i++) {
-  var userXml = users.ele('user');
-  userXml.ele('id',  usersObj[i].id );
-  userXml.ele('name',  usersObj[i].name);
-  userXml.ele('email',  usersObj[i].email);
-}
-  
+const db = new sqlite3.Database(dbPath);
+var builder = require("xmlbuilder");
+let fs = require("fs");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
+  var users = builder.create("users", { encoding: "utf-8" });
+  users.att("xmlns", "http://www.w3.org/2005/Atom");
   db.serialize(function() {
-    db.all(`SELECT * FROM users`, function(err, rows){
-      console.log(rows);
+    db.all(`SELECT * FROM users ORDER BY id ASC`, function(err, rows) {
+      for (var i = 0; i < rows.length; i++) {
+        var userXml = users.ele("user");
+        userXml.ele("id", rows[i].id);
+        userXml.ele("name", rows[i].name);
+        userXml.ele("email", rows[i].email);
+      }
+
+      res.header("Content-Type", "text/xml");
+      res.send(userXml.end({ pretty: true }));
     });
   });
- res.header('Content-Type', 'text/xml');
- res.send(users.end({ pretty: true}));
 });
 
-
-router.post('/', function(req, res, next) {
-  
+router.get("/download", function(req, res, next) {
+  var users = builder.create("users", { encoding: "utf-8" });
+  users.att("xmlns", "http://www.w3.org/2005/Atom");
   db.serialize(function() {
-      db.run(`INSERT INTO users (name, email)  
-              VALUES ('${req.body.name}', '${req.body.email}');`
-      );
+    db.all(`SELECT * FROM users ORDER BY id ASC`, function(err, rows) {
+      for (var i = 0; i < rows.length; i++) {
+        var userXml = users.ele("user");
+        userXml.ele("id", rows[i].id);
+        userXml.ele("name", rows[i].name);
+        userXml.ele("email", rows[i].email);
+      }
+
+      let filename = "users.xml";
+      let absPath = path.join(__dirname, "../generated/", filename);
+      let relPath = path.join("../generated", filename);
+      //res.setHeader('Content-disposition', 'attachment; filename=teste');
+      //res.setHeader("Content-Type", "text/xml");
+      //res.send(userXml.end({ pretty: true }));
+
+      fs.writeFile(relPath, "File content", err => {
+        if (err) {
+          console.log(err);
+        }
+        res.download(absPath, err => {
+          if (err) {
+            console.log(err);
+          }
+          fs.unlink(relPath, err => {
+            if (err) {
+              console.log(err);
+            }
+            console.log("FILE [" + filename + "] REMOVED!");
+          });
+        });
+      });
+    });
   });
-  res.send();
 });
 
-
-router.get('/:id', function(req, res, next) {
-  
-  var user = builder.create('user', { encoding: 'utf-8' });
-  user.att('xmlns', 'http://www.w3.org/2005/Atom');
-  
-  var userObj = usersObj.filter(function(userFilter) {
-    return userFilter.id == req.params.id;
+router.post("/", function(req, res, next) {
+  db.serialize(function() {
+    db.run(
+      `INSERT INTO users (name, email)  VALUES ('${req.body.name}', '${req.body.email}');`
+    );
+    res.send();
   });
+});
 
-  var userXml = user.ele('user');
-  userXml.ele('id',  userObj[0].id );
-  userXml.ele('name',  userObj[0].name);
-  userXml.ele('email',  userObj[0].email);
+router.get("/:id", function(req, res, next) {
+  var user = builder.create("user", { encoding: "utf-8" });
+  user.att("xmlns", "http://www.w3.org/2005/Atom");
 
-  //console.log(userObj);
+  db.serialize(function() {
+    db.all(`SELECT * FROM users WHERE id = ${req.params.id}`, function(
+      err,
+      row
+    ) {
+      console.log(row);
+      var userXml = user.ele("user");
+      userXml.ele("id", row[0].id);
+      userXml.ele("name", row[0].name);
+      userXml.ele("email", row[0].email);
 
- res.header('Content-Type', 'text/xml');
- res.send(userXml.end({ pretty: true}));
+      res.header("Content-Type", "text/xml");
+      res.send(userXml.end({ pretty: true }));
+    });
+  });
+});
+
+router.put("/:id", function(req, res, next) {
+  var user = builder.create("user", { encoding: "utf-8" });
+  user.att("xmlns", "http://www.w3.org/2005/Atom");
+
+  db.serialize(function() {
+    db.run(
+      `UPDATE users SET name='${req.body.name}', email='${req.body.email}' WHERE id = ${req.params.id}`,
+      function(err, row) {
+        res.send();
+      }
+    );
+  });
+});
+
+router.delete("/:id", function(req, res, next) {
+  var user = builder.create("user", { encoding: "utf-8" });
+  user.att("xmlns", "http://www.w3.org/2005/Atom");
+
+  db.serialize(function() {
+    db.run(`DELETE FROM users WHERE id = ${req.params.id}`, function(err, row) {
+      res.send();
+    });
+  });
 });
 
 module.exports = router;
